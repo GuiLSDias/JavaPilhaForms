cf223a98f52d7447223b89be693020b6caa2f184
GuiLSDias <127917228+GuiLSDias@users.noreply.github.com>
2024-03-26 12:24:09 -0300

    Add files via upload

---
+++ javarecordscontrol/src/pilha/JavaRecorde.java
@@ -0,0 +1,13 @@
+
+package pilha;
+
+
+public class JavaRecorde {
+
+
+    public static void main(String[] args) {
+        FormSistema f = new FormSistema();
+        f.setVisible(true);
+    }
+    
+}
